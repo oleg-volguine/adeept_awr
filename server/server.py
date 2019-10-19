2faeb08ea02bb16f251f624f8c348f84b50fd24f
Oleg Volguine <oleg.volguine@gmail.com>
2019-10-19 23:05:28 +1100

    test

--- server/server.py
+++ server/server.py
@@ -146,7 +146,7 @@ def ap_thread():
     os.system("sudo create_ap wlan0 eth0 AdeeptCar 12345678")
 
 
-def blink_led(self, LED, color):
+def blink_led(LED, color):
     while (client_connected == False):
         #LED = LED.LED()
         LED.blink(color)
@@ -335,7 +335,7 @@ if __name__ == '__main__':
 
             print('waiting for connection...')
             # Blink LED blue colour..waiting for connection 
-            blink_led_threading=threading.Thread(target=blink_led, args=(self,LED,Color(0,239,255),))
+            blink_led_threading=threading.Thread(target=blink_led, args=(LED,Color(0,239,255),))
             blink_led_threading.setDaemon(True)                             
             blink_led_threading.start()
             
